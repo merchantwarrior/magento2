0f787a33ea6f3b66a46f1519bab984bea0b820e5
Omair Khalid <omair.khalid@overdose.digital>
2022-10-25 19:35:32 +0000

    Removed payframe lib from module and referenced it via link on request of client.

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,7 +1,7 @@
 var config = {
     map: {
         '*': {
-            payframeLib: 'MerchantWarrior_Payment/js/lib/payframe'
+            payframeLib: 'https://secure.merchantwarrior.com/payframe/payframe.js'
         }
     }
 };
